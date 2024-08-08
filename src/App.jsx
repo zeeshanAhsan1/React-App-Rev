f81bd69260b01875fdf9144fde76125e82bb93b9
Zeeshan Ahsan <zeeshan.ahsan15@gmail.com>
2024-08-07 23:35:41 -0500

    Refactor code for modularity.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,5 @@
-import reactImg from './assets/react-core-concepts.png';
-import { CORE_CONCEPTS } from './data.js';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
+import { CORE_CONCEPTS } from "./data.js";
+import Header from "./components/Header.jsx";
 
 function CoreConcept({ image, title, description }) {
   return (
