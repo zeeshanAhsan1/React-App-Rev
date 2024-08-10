81e1ac7bdccf480622d04c658ef663bc2b7a2287
Zeeshan Ahsan <zeeshan.ahsan15@gmail.com>
2024-08-10 18:53:21 -0500

    Components can't be re-rendered using just vars. Need State for that.

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,13 @@ import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  let tabContent = "Please click a button";
+
   function handleSelect(selectedButton) {
     // selectedButton => 'components', 'jsx', 'props', 'state'
-    console.log(selectedButton);
+    // console.log(selectedButton);
+    tabContent = selectedButton;
+    console.log(tabContent);
   }
 
   return (
@@ -36,6 +40,7 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
+          {tabContent}
         </section>
       </main>
     </div>
